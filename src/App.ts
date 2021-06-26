1894114ca416c6db740bd06cf4fe782bb2fa9fcb
大井さかな <41501869+01397@users.noreply.github.com>
2021-06-26 20:23:55 +0900

    リリース v0.4.2

--- src/App.ts
+++ src/App.ts
@@ -93,7 +93,7 @@ export default class App {
   private menu: MenuItem[]
 
   constructor(root: Element) {
-    this.version = '0.4.1'
+    this.version = '0.4.2'
     this.sidebarElm = h('aside', { id: 'sidebar' }, null)
     this.toolbarElm = h('div', { id: 'toolbar' }, null)
     this.tabbarElm = h('div', { id: 'tabbar' }, null)
