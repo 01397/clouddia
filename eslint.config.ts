85e40dfc447d73acbc583297ead79ced9f2914be
01397 <41501869+01397@users.noreply.github.com>
2025-03-11 01:46:48 +0900

    update eslint version from 8 to 9

---
+++ eslint.config.ts
@@ -0,0 +1,33 @@
+// @ts-check
+import js from '@eslint/js';
+import tseslint from "typescript-eslint";
+import tseslintParser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config([
+  {
+    ignores: ['dist'],
+  },
+  {
+    files: ['src/**/*.ts'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
+    languageOptions: {
+      parser: tseslintParser,
+      parserOptions: {
+        ecmaVersion: 2024,
+        sourceType: 'module',
+        project: true,
+      },
+    },
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-prototype-builtins': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  eslintConfigPrettier,
+]);
